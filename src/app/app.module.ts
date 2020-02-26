c9a179d9085991856d982ea451f7acc5b7f6272a
Tata.Gaspar Peña <agasparp@proveedor.intercorp.com.pe>
2020-02-25 19:06:08 -0500

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { Page2Component } from './component/page2/page2.component';
 import { Page3Component } from './component/page3/page3.component';
 import { CommonModule} from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './componentHeroes/home/home.component';
+import { AboutComponent } from './componentHeroes/about/about.component';
+import { HeroesComponent } from './componentHeroes/heroes/heroes.component';
+import { NavbarComponent } from './componentHeroes/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
     Page2Component,
-    Page3Component
+    Page3Component,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
